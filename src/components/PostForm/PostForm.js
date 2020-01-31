06930ce58a6452c8e86388f1da248d027675f203
Luciano <luchosr@gmail.com>
2020-01-31 11:59:40 -0300

    firs commit

---
+++ src/components/PostForm/PostForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PostForm = () => {
+  return (
+    <div>
+      <h1>Create Post</h1>
+      <form action="">
+        <input required type="text" placeholder="Enter Post Title" /> <br />
+        <br />
+        <textarea required name="" id="" cols="28" rows="5"></textarea>
+        <button></button>
+      </form>
+    </div>
+  );
+};
